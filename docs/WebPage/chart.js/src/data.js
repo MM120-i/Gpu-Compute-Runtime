7a06f0676c0351e2e6272b22245eda808d02fe81
Mahim <mahimmarufuzzaman1234@gmail.com>
2026-06-25 14:45:25 -0400

    Github Actions

--- docs/WebPage/chart.js/src/data.js
+++ docs/WebPage/chart.js/src/data.js
@@ -1,5 +1,5 @@
 export const fetchBenchData = async () => {
-    const res = await fetch("/bench_results.json");
+    const res = await fetch("bench_results.json");
 
     if(!res.ok)
         throw new Error(`Failed to load bench_results.json : ${res.status}`);
